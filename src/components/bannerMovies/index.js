e6ec886c36608056668b80af3ac54079b28912aa
unknown <you@example.com>
2023-06-02 17:38:12 -0300

    projeto filmes

---
+++ src/components/bannerMovies/index.js
@@ -0,0 +1,13 @@
+import React,{Fragment} from 'react'
+import {Image,Text} from 'react-native'
+import styles from './styles';
+
+export default function BannerMovies(){
+    const imagem = Math.floor(Math.random()*4 +1)
+    return(
+            <>
+            <Text style = {styles.textBanner}>Em cartaz</Text>
+            <Image style = {styles.imageBanner} source = {require(`../../Img/${imagem}.jpg`)} />
+            </>
+    );
+}
---
+++ src/components/datas/filmes.js
@@ -0,0 +1,67 @@
+const DATA =
+[
+    {
+        id:'01',
+        nome:'Adão Negro',
+        nota:'4.5',
+        imagem:'adao.jpg'
+    },
+    {
+        id: '02',
+        nome:'Avatar',
+        nota:'9.0',
+        imagem:'avatar.jpg',
+    },
+    {
+        id: '03',
+        nome:'Desaparecida',
+        nota:'8.5',
+        imagem:'desaparecida.jpg',
+    },
+    {
+        id: '04',
+        nome:'Gato de Botas II',
+        nota:'9.8',
+        imagem:'gato.jpg',
+    },
+    {
+        id: '05',
+        nome:'Homem Formiga',
+        nota:'8.0',
+        imagem:'homemformiga.jpg',
+    },
+    {
+        id: '06',
+        nome:'Os Incríveis',
+        nota:'9.8',
+        imagem:'incríveis.jpg',
+    },
+    {
+        id: '07',
+        nome:'Mario',
+        nota:'9.8',
+        imagem:'mario.jpg',
+    },
+    {
+        id: '08',
+        nome:'Megan',
+        nota:'8.0',
+        imagem:'megan.jpg',
+    },
+    {
+        id: '09',
+        nome:'Pânico',
+        nota:'9.0',
+        imagem:'panico.jpg',
+    },
+    {
+        id: '10',
+        nome:'Wandinha',
+        nota:'7.0',
+        imagem:'wnadinha.jpg',
+    },
+
+
+]
+
+export default DATA;
